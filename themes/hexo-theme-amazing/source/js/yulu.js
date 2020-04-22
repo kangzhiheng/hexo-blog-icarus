15124da28098d0f9a6f0f02dd7b4e8ce6f91067a
kangzhiheng <kangzhiheng@live.cn>
2020-04-22 14:22:08 +0800

    commit theme

---
+++ themes/hexo-theme-amazing/source/js/yulu.js
@@ -0,0 +1,12 @@
+$(function () { // 获取记录数据
+    $.getJSON("../json_data/yulu.json", function (data) {
+        $.each(data, function (i, e) {
+            var html = '<li class="time-axis-item">' +
+                '<div class="time-axis-date">' + e.date + '<span></span></div>' +
+                '<div class="time-axis-title">' + e.title + '</div>' +
+                '<p class="time-axis-achievement">' + e.achievement + '</p>' +
+                '</li>';
+            $('.time-axis').append(html);
+        });
+    })
+});
